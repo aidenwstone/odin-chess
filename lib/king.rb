8628004e86b0146e8cfbbb5a569e29c14146f50c
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-13 18:59:50 -0400

    Add King class

---
+++ lib/king.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require './lib/piece'
+
+# The King subclass manages a King chess piece.
+# It implements the abstract methods from the Piece superclass.
+class King < Piece
+end
