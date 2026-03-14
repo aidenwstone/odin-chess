dbc5fbaed56ccbd3868f05bba0269b8870624cc5
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-14 18:11:59 -0400

    Add Knight class

---
+++ lib/knight.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require './lib/piece'
+
+# The Knight subclass manages a Knight chess piece.
+# It implements the abstract methods from the Piece superclass.
+class Knight < Piece
+end
