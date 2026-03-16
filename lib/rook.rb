aae229a12605bc40bee33b26db0684b3a1e83388
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-16 18:40:15 -0400

    Implement Rook#to_s method

--- lib/rook.rb
+++ lib/rook.rb
@@ -5,4 +5,7 @@ require './lib/piece'
 # The Rook subclass manages a Rook chess piece.
 # It implements the abstract methods from the Piece superclass.
 class Rook < Piece
+  def to_s
+    white? ? "\u2656" : "\u265C"
+  end
 end
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -3,4 +3,21 @@
 require './lib/rook'
 
 describe Rook do
+  describe '#to_s' do
+    context 'when the rook is white' do
+      subject(:white_rook) { described_class.new(true) }
+
+      it "returns the white rook unicode symbol (\u2656)" do
+        expect(white_rook.to_s).to be("\u2656")
+      end
+    end
+
+    context 'when the rook is black' do
+      subject(:black_rook) { described_class.new(false) }
+
+      it "returns the black rook unicode symbol (\u265C)" do
+        expect(black_rook.to_s).to be("\u265C")
+      end
+    end
+  end
 end
