98e3cd922954fb862e4e89cffa3dc6ca3e2285d8
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-16 16:44:41 -0400

    Implement Knight#to_s method

--- lib/knight.rb
+++ lib/knight.rb
@@ -5,4 +5,7 @@ require './lib/piece'
 # The Knight subclass manages a Knight chess piece.
 # It implements the abstract methods from the Piece superclass.
 class Knight < Piece
+  def to_s
+    white? ? "\u2658" : "\u265E"
+  end
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -3,4 +3,21 @@
 require './lib/knight'
 
 describe Knight do
+  describe '#to_s' do
+    context 'when the knight is white' do
+      subject(:white_knight) { described_class.new(true) }
+
+      it "returns the white knight unicode symbol (\u2658)" do
+        expect(white_knight.to_s).to be("\u2658")
+      end
+    end
+
+    context 'when the knight is black' do
+      subject(:black_knight) { described_class.new(false) }
+
+      it "returns the black knight unicode symbol (\u265E)" do
+        expect(black_knight.to_s).to be("\u265E")
+      end
+    end
+  end
 end
