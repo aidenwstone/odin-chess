44689bf5adfb04246a98ed5120a53f35826dc422
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-16 18:38:24 -0400

    Add Rook class

---
+++ lib/rook.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require './lib/piece'
+
+# The Rook subclass manages a Rook chess piece.
+# It implements the abstract methods from the Piece superclass.
+class Rook < Piece
+end
