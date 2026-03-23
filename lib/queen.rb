cd4b11edccf824c82253c1df8cd1face6b61b0fe
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-23 17:40:31 -0400

    Add Queen class

---
+++ lib/queen.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require './lib/piece'
+
+# The Queen subclass manages a Queen chess piece.
+# It implements the abstract methods from the Piece superclass.
+class Queen < Piece
+end
---
+++ spec/queen_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require './lib/queen'
+
+describe Queen do
+end
