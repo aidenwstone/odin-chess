2acfed81e3d8c9dc84c87e940c621c845aca063f
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-23 17:46:40 -0400

    Implement Queen#to_s

--- lib/queen.rb
+++ lib/queen.rb
@@ -5,4 +5,7 @@ require './lib/piece'
 # The Queen subclass manages a Queen chess piece.
 # It implements the abstract methods from the Piece superclass.
 class Queen < Piece
+  def to_s
+    white? ? "\u2655" : "\u265B"
+  end
 end
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -3,4 +3,21 @@
 require './lib/queen'
 
 describe Queen do
+  describe '#to_s' do
+    context 'when the queen is white' do
+      subject(:white_queen) { described_class.new(true) }
+
+      it "returns a white queen unicode symbol (\u2655)" do
+        expect(white_queen.to_s).to be("\u2655")
+      end
+    end
+
+    context 'when the queen is black' do
+      subject(:black_queen) { described_class.new(false) }
+
+      it "returns a black queen unicode symbol (\u265B)" do
+        expect(black_queen.to_s).to be("\u265B")
+      end
+    end
+  end
 end
