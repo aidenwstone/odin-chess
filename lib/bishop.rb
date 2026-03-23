8fbfccfee775ffcf6427b47ff43ea111a6f0d50e
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-23 16:14:44 -0400

    Add Bishop class

---
+++ lib/bishop.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require './lib/piece'
+
+# The Bishop subclass manages a Bishop chess piece.
+# It implements the abstract methods from the Piece superclass.
+class Bishop < Piece
+end
