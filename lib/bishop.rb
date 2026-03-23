7bc9e475aab07222bfb815ff1cd01324a0c1f4ff
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-23 16:28:31 -0400

    Implement Bishop#to_s method

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -5,4 +5,7 @@ require './lib/piece'
 # The Bishop subclass manages a Bishop chess piece.
 # It implements the abstract methods from the Piece superclass.
 class Bishop < Piece
+  def to_s
+    white? ? "\u2657" : "\u265D"
+  end
 end
--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -3,4 +3,21 @@
 require './lib/bishop'
 
 describe Bishop do
+  describe '#to_s' do
+    context 'when the bishop is white' do
+      subject(:white_bishop) { described_class.new(true) }
+
+      it "returns the white bishop unicode symbol (\u2657)" do
+        expect(white_bishop.to_s).to be("\u2657")
+      end
+    end
+
+    context 'when the bishop is black' do
+      subject(:black_bishop) { described_class.new(false) }
+
+      it "returns the black bishop unicode symbol (\u265D)" do
+        expect(black_bishop.to_s).to be("\u265D")
+      end
+    end
+  end
 end
