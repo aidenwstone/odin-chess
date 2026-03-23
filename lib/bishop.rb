af29fb5b17e32d610cae576fda8250db57c7d76f
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-03-23 17:36:18 -0400

    Implement Bishop#moves

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -5,7 +5,17 @@ require './lib/piece'
 # The Bishop subclass manages a Bishop chess piece.
 # It implements the abstract methods from the Piece superclass.
 class Bishop < Piece
+  AVAILABLE_MOVES = [[-7, 7], [-6, 6], [-5, 5], [-4, 4], [-3, 3], [-2, 2], [-1, 1],
+                     [1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7],
+                     [7, 7], [6, 6], [5, 5], [4, 4], [3, 3], [2, 2], [1, 1],
+                     [-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7]]
+                    .freeze
+
   def to_s
     white? ? "\u2657" : "\u265D"
+  end
+
+  def moves
+    AVAILABLE_MOVES
   end
 end
--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -3,6 +3,13 @@
 require './lib/bishop'
 
 describe Bishop do
+  let(:movement_vectors) do
+    [[-7, 7], [-6, 6], [-5, 5], [-4, 4], [-3, 3], [-2, 2], [-1, 1],
+     [1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7],
+     [7, 7], [6, 6], [5, 5], [4, 4], [3, 3], [2, 2], [1, 1],
+     [-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7]]
+  end
+
   describe '#to_s' do
     context 'when the bishop is white' do
       subject(:white_bishop) { described_class.new(true) }
@@ -18,6 +25,14 @@ describe Bishop do
       it "returns the black bishop unicode symbol (\u265D)" do
         expect(black_bishop.to_s).to be("\u265D")
       end
+    end
+  end
+
+  describe '#moves' do
+    subject(:bishop) { described_class.new(true) }
+
+    it 'returns the diagonal movement vectors' do
+      expect(bishop.moves).to match_array(movement_vectors)
     end
   end
 end
