73351071f8b4138ebd6a37fc29e635b50f58d2db
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-04-17 15:53:03 -0400

    Implement Game#choose_start_square method

--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,9 @@ require './lib/board'
 # The Game class manages a game of chess, keeping track of the current player, asking for input,
 # and announcing a winner. It also handles the saving/loading of games.
 class Game
+  NOTATION_FORMAT = /(?<file>[a-h])(?<rank>[1-8])/.freeze
+  FILE_TO_NUM = ('a'..'h').each_with_index.to_h
+
   attr_reader :board, :current_player
 
   def initialize
@@ -14,5 +17,37 @@ class Game
 
   def switch_player
     @current_player = current_player == :white ? :black : :white
+  end
+
+  def choose_start_square
+    loop do
+      notation_selection = ask_for_input('Please select which piece to move using chess notation (e.g. b3 or f5):')
+      square = notation_to_coordinates(notation_selection)
+
+      return square unless square.nil? || board.legal_moves(square).empty?
+
+      puts 'Invalid selection, please try again.'
+    end
+  end
+
+  def choose_target_square(start_square)
+  end
+
+  private
+
+  def ask_for_input(message)
+    puts message
+    gets.chomp
+  end
+
+  def notation_to_coordinates(notation)
+    match_data = notation.match(NOTATION_FORMAT)
+
+    return if match_data.nil?
+
+    column = FILE_TO_NUM[match_data[:file]]
+    row = match_data[:rank].to_i - 1
+
+    [row, column]
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,9 +3,9 @@
 require './lib/game'
 
 describe Game do
-  describe '#switch_player' do
-    subject(:game) { described_class.new }
+  subject(:game) { described_class.new }
 
+  describe '#switch_player' do
     context 'when white is the current player' do
       it 'changes the current player to black' do
         expect { game.switch_player }.to change(game, :current_player).from(:white).to(:black)
@@ -19,6 +19,68 @@ describe Game do
 
       it 'changes the current player to white' do
         expect { game.switch_player }.to change(game, :current_player).from(:black).to(:white)
+      end
+    end
+  end
+
+  describe '#choose_start_square' do
+    before do
+      allow(game).to receive(:puts) # rubocop:disable RSpec/SubjectStub
+    end
+
+    context 'when the player chooses a valid square' do
+      let(:start_square) { [1, 3] }
+      let(:input_valid) { "d2\n" }
+
+      before do
+        allow(game).to receive(:ask_for_input).and_return(input_valid) # rubocop:disable RSpec/SubjectStub
+      end
+
+      it 'requests input once' do
+        game.choose_start_square
+        expect(game).to have_received(:ask_for_input).once # rubocop:disable RSpec/SubjectStub
+      end
+
+      it 'returns the valid square coordinates' do
+        expect(game.choose_start_square).to eq(start_square)
+      end
+    end
+
+    context 'when the player chooses an invalid square, then a valid square' do
+      let(:start_square) { [1, 6] }
+      let(:input_invalid) { "f1\n" }
+      let(:input_valid) { "g2\n" }
+
+      before do
+        allow(game).to receive(:ask_for_input).and_return(input_invalid, input_valid) # rubocop:disable RSpec/SubjectStub
+      end
+
+      it 'requests input twice' do
+        game.choose_start_square
+        expect(game).to have_received(:ask_for_input).twice # rubocop:disable RSpec/SubjectStub
+      end
+
+      it 'returns the valid square coordinates' do
+        expect(game.choose_start_square).to eq(start_square)
+      end
+    end
+
+    context 'when the player enters bad input once, then chooses a valid square' do
+      let(:start_square) { [0, 6] }
+      let(:input_bad) { "bad input\n" }
+      let(:input_valid) { "g1\n" }
+
+      before do
+        allow(game).to receive(:ask_for_input).and_return(input_bad, input_valid) # rubocop:disable RSpec/SubjectStub
+      end
+
+      it 'requests input twice' do
+        game.choose_start_square
+        expect(game).to have_received(:ask_for_input).twice # rubocop:disable RSpec/SubjectStub
+      end
+
+      it 'returns the valid square coordinates' do
+        expect(game.choose_start_square).to eq(start_square)
       end
     end
   end
