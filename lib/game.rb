77c61787f556b252d95ab09d6bc75b45b7c2db1f
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-04-28 16:48:36 -0400

    Update Game#choose_target_square prompt message

--- lib/game.rb
+++ lib/game.rb
@@ -39,7 +39,7 @@ class Game
 
   def choose_target_square(start_square)
     loop do
-      notation_selection = ask_for_input("#{current_player.capitalize}, please select which square to move to using chess notation (e.g. b3 or f5):") # rubocop:disable Layout/LineLength
+      notation_selection = ask_for_input('Now select which square to move to:')
       square = notation_to_coordinates(notation_selection)
       legal_moves = board.legal_moves(start_square)
 
